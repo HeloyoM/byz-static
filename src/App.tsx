e8632fe5e5e01326930e2173eba8f99de640b8b8
unknown <mybs2323@gmail.com>
2025-02-04 11:12:27 +0200

    added client id

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import GoogleAuthButton from 'components/GoogleAuthButton';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <h1>Welcome to the byl Website</h1>
+        <GoogleAuthButton />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="734653271878-dlrb5umuqm2g9nb3lp88e7c3rcf2j65m.apps.googleusercontent.com">
+        <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
