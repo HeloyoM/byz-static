23c52a3cb2d693312bd2effbb782e68f670e6e8a
unknown <mybs2323@gmail.com>
2025-02-25 15:04:00 +0200

    setup 0auth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,39 @@ import Menu from 'components/common/Menu';
 import AppList from 'components/common/AppList';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+    return (<button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+        Log Out
+    </button>)
+}
 
 const Header = () => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
     const [isSticky, setSticky] = React.useState(false)
     const [openMenu, setOpenMenu] = React.useState(false)
 
     const openMenuModal = () => { setOpenMenu(true) }
     const closeMenuModal = () => { setOpenMenu(false) }
-
+    console.log({ user })
     const optionsListItems: IListItem[] = [
         { primary: 'galery', handleClick: () => { }, secondary: '' },
         {
             primary: 'active', handleClick: () => { }, secondary: ''
+        },
+        {
+            primary: <LoginButton />, handleClick: () => { }, secondary: ''
+        },
+        {
+            primary: <LogoutButton />, handleClick: () => { }, secondary: ''
         }
     ]
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="734653271878-dlrb5umuqm2g9nb3lp88e7c3rcf2j65m.apps.googleusercontent.com">
-        <App />
-    </GoogleOAuthProvider>
+    {/* <GoogleOAuthProvider clientId="734653271878-dlrb5umuqm2g9nb3lp88e7c3rcf2j65m.apps.googleusercontent.com"> */}
+    <Auth0Provider
+      domain="dev-4jb37adw774rzacm.us.auth0.com"
+      clientId="JGfv5r647ow3sI7MCJ0lMnzPM9a3SvYg"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    {/* </GoogleOAuthProvider> */}
   </React.StrictMode>
 );
 
