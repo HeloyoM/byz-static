17e11b26faad6a28b60ba1c5cc9e01f8bb48e545
unknown <mybs2323@gmail.com>
2025-02-25 13:22:33 +0200

    header has added

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,15 @@ import Profile from 'pages/Profile';
 import Gallery from 'pages/Gallery';
 import Home from 'pages/Home';
 import ScreenWrapper from 'components/ScreenWrapper';
-import Menu from 'components/common/Menu';
-import { IListItem } from 'interface/IListItem.interface';
-import AppList from 'components/common/AppList';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
 import './App.css';
+import Header from 'components/Header';
 
 const App: React.FC = () => {
-  const [openMenu, setOpenMenu] = useState(false)
 
-  const openMenuModal = () => { setOpenMenu(true) }
-  const closeMenuModal = () => { setOpenMenu(false) }
-
-  const optionsListItems: IListItem[] = [
-    { primary: 'galery', handleClick: () => { }, secondary: '' },
-    {
-      primary: 'active', handleClick: () => { }, secondary: ''
-    }
-  ]
 
   return (
     <Router>
-      {openMenu ? <CloseIcon className="menu-btn" onClick={closeMenuModal} /> : <MenuIcon onClick={openMenuModal} className="menu-btn" />}
-
-      <Menu menuBody={<AppList items={optionsListItems} />} close={closeMenuModal} openMenu={openMenu} />
-
+      <Header />
       <Routes>
         <Route path="/" element={<ScreenWrapper ><Home /></ScreenWrapper>} />
         <Route path="/profile" element={<Profile />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Typography } from "@mui/material";
+import '../App.css';
+import { IListItem } from 'interface/IListItem.interface';
+import Menu from 'components/common/Menu';
+import AppList from 'components/common/AppList';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
+const Header = () => {
+    const [isSticky, setSticky] = React.useState(false)
+    const [openMenu, setOpenMenu] = React.useState(false)
+
+    const openMenuModal = () => { setOpenMenu(true) }
+    const closeMenuModal = () => { setOpenMenu(false) }
+
+    const optionsListItems: IListItem[] = [
+        { primary: 'galery', handleClick: () => { }, secondary: '' },
+        {
+            primary: 'active', handleClick: () => { }, secondary: ''
+        }
+    ]
+    React.useEffect(() => {
+
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setSticky(true)
+            } else {
+                setSticky(false)
+            }
+        }
+
+
+        window.addEventListener('scroll', handleScroll)
+
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    return (
+        <Typography className={isSticky ? 'title sticky' : 'title'}>
+            <Typography style={{ margin: 'auto auto' }}>BYL</Typography>
+            {openMenu ? <CloseIcon className="menu-btn" onClick={closeMenuModal} /> : <MenuIcon onClick={openMenuModal} className="menu-btn" />}
+
+            <Menu menuBody={<AppList items={optionsListItems} />} close={closeMenuModal} openMenu={openMenu} />
+
+        </Typography>
+    )
+}
+
+export default Header;
